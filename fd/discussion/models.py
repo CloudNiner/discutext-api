317bbf1e2493eac024817cfe1dcadf226eb3ba88
CloudNiner <andy@finkmiller.com>
2020-01-17 16:48:11 -0500

    Further split sections into paragraphs
    BREAKING CHANGE. Response structure replaces
    `section.text: String` with `section.paragraphs: Array[String]`.
    
    Discussions have gotten a bit more consistently
    structured. The new strategy generally is:
    
    - Split on &&: This is the section delimiter
    - Look for a header at the start of each section
      with the format ".<headertext>...". If not found,
      header value is empty string
    - Split section text into paragraphs on double newlines (\n\n)
    - Cleanup paragraph text to remove remaining newlines and reduce
      duplicate whitespace.

--- fd/discussion/models.py
+++ fd/discussion/models.py
@@ -7,7 +7,7 @@ logger = logging.getLogger('fd.discussion')
 
 class ForecastDiscussion(object):
     """ ForecastDiscussion, unique on (wfo, valid_at)."""
-    TEXT_REGEX = r'  \.(?P<header>[\w\s/]+)\.{3} (?P<text>.+?)  &&'
+    HEADER_REGEX = re.compile(r'\.(?P<header>[\w \/]+)\.{3}(?P<text>.*)', re.DOTALL)
 
     def __init__(self, wfo_id, valid_at_str, text):
         self.text = text
@@ -28,9 +28,32 @@ class ForecastDiscussion(object):
         valid_dt = dateutil.parser.parse(iso_str)
         return int(valid_dt.timestamp())
 
+    def _clean_section_text(self, text):
+        """ Split into paragraphs.
+
+        Steps:
+            - Replace newlines separated by only zero or more whitespace with double newlines
+            - Split on double newlines
+            - For each paragraph:
+              - Replace each remaining newline with empty space
+              - Reduce each occurence of one or more spaces with one space
+              - Strip whitespace from ends of paragraph
+        """
+        paragraphs = re.sub(r'\n\s*\n', '\n\n', text).split('\n\n')
+        return [re.sub(r' +', " ", p.replace("\n", " ")).strip() for p in paragraphs if p]
+
     def _parse_text(self):
         if self.text is None or len(self.text) == 0:
             return None
-        cleaned_text = self.text.replace('\n', ' ')
-        return [match.groupdict() for match in re.finditer(self.TEXT_REGEX, cleaned_text)
-                    if match is not None]
+        sections = self.text.split("&&")
+        matches = []
+        for section in sections:
+            match = self.HEADER_REGEX.search(section)
+            if match is not None:
+                matches.append(match.groupdict())
+            else:
+                matches.append({'header': '', 'text': section})
+        return [{
+            'header': m.get('header', ''),
+            'paragraphs': self._clean_section_text(m.get('text', ''))
+        } for m in matches]
